61cf6ffa9a8178257f1701f601d57675b28e11ab
nevets615 <surj316@yahoo.com>
2019-02-22 12:57:38 -0500

    i have worked on both the cards and the tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
-const Tab = props => {
+const Tab = FirstTab => SecondTab => props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      if (tab(selectedTab)) {
+       
+        return <tab active-tab />;
+      }
+      
+      return <tab />;
+    };
+     
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
+    const selectTabHandler = tab => props => {
+      onclick{props.tab};
+    }
+      >
+      /* Replace this dummy click handler function with your selectTabHandler function from props 
+       you'll need to pass the `tab` in as an argument to this handler. */
       {props.tab.toUpperCase()}
     </div>
   );
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -4,8 +4,8 @@ const Tabs = props => {
   return (
     {props.tabs.map(tabs => (
     <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+      <div className={tabs.topics}>
+        <span className={tabs.title}>TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
@@ -15,4 +15,8 @@ const Tabs = props => {
           }
 
 // Make sure to use PropTypes to validate your types!
+
+Tabs.propTypes = {
+  tabs:  PropTypes.string.isRequired
+}
 export default Tabs;
