fe828279ed821464798de53e7f6e83d4fd839c6e
nevets615 <surj316@yahoo.com>
2019-03-29 12:05:39 -0400

    working on the filter function in content

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,12 +4,24 @@ import Card from './Card';
 const Cards = props => {
   return (
     <div className="cards-container">
+     {props.cards.map((item, index) => ( 
+        <Card key={index} card={item} />
+      ))}
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
+  );
+};
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      headline: PropTypes.string.isRequired,
+      tab: PropTypes.string.isRequired,
+      img: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired
+    })
   )
-}
-
+};
 // Make sure you include prop types for all of your incoming props
 
 export default Cards;
